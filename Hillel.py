ae511d63e607fdbbc3d4eeb8b5d0b90f6fac4b33
Veronika <nickatheowl@gmail.com>
2025-06-23 16:18:11 +0300

    first change to document

--- Hillel.py
+++ Hillel.py
@@ -19,3 +19,23 @@ d5 = number % 10
 reversed_number = d5 * 10000 + d4 * 1000 + d3 * 100 + d2 * 10 + d1
 print(reversed_number)
 
+num1 = float(input("Введіть перше число: ")) #Урок3.1
+operation = input("Введіть операцію (+, -, *, /): ")
+num2 = float(input("Введіть друге число: "))
+if operation == '+':
+    result = num1 + num2
+    print("Результат:", result)
+elif operation == '-':
+    result = num1 - num2
+    print("Результат:", result)
+elif operation == '*':
+    result = num1 * num2
+    print("Результат:", result)
+elif operation == '/':
+    if num2 == 0:
+        print("Помилка: Ділення на нуль!")
+    else:
+        result = num1 / num2
+        print("Результат:", result)
+else:
+    print("Невідома операція!")
