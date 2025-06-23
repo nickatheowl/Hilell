1dde03de8e4f3f91d53d95d6b6d8a0f832799460
Veronika <nickatheowl@gmail.com>
2025-06-23 16:02:23 +0300

    first change to document

--- Hillel.py
+++ Hillel.py
@@ -1,2 +1,14 @@
-print ("Hello World")
+print("Hello World")  #Урок1
+
+number = int(input("Введіть 4-значне число: "))
+
+digit1 = number // 1000
+digit2 = (number % 1000) // 100
+digit3 = (number % 100) // 10
+digit4 = number % 10
+
+print(digit1)
+print(digit2)
+print(digit3)
+print(digit4)
 
