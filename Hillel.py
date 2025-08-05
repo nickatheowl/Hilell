c00621e05989745bf871562857792fd20941bc7c
Veronika <nickatheowl@gmail.com>
2025-08-05 18:01:57 +0300

    first change to document

--- Hillel.py
+++ Hillel.py
@@ -84,20 +84,24 @@ lst = [random.randint(1, 9) for _ in range(random.randint(3, 10))]
 new_lst = [lst[0], lst[2], lst[-2]]
 print(new_lst)
 
+
 import string #Урок 5.1
 import keyword
 
-my_string = input("I like pizza")
+my_string = "pizza_time"
 
 print(my_string)
 print(type(my_string))
 
 print(
-    (my_string != '') and
-    (not my_string[0].isdigit()) and
-    (my_string == my_string.lower()) and
-    (all((c not in string.punctuation or c == '_') and not c.isspace() for c in my_string)) and
-    (my_string not in keyword.kwlist) and
-    (my_string.count('_') <= 1) and
-    (not (set(my_string) == {'_'} and len(my_string) > 1))
+    not keyword.iskeyword(my_string)
+    and not (my_string.count('_') > 1 and all(c == '_' for c in my_string))
+    and my_string != ''
+    and not my_string[0].isdigit()
+    and all(
+        c not in string.punctuation.replace('_', '')
+        and not c.isupper()
+        and not c.isspace()
+        for c in my_string
+    )
 )
