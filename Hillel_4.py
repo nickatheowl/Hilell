ec76bbb3ffa2f1bd0eb46d864c5b0e18b7afe965
Veronika <nickatheowl@gmail.com>
2025-08-09 20:36:49 +0300

    #Урок 10.2

--- Hillel_4.py
+++ Hillel_4.py
@@ -15,3 +15,18 @@ def greet_veronika(username):
     print(f"Привіт, {username}!")
 
 greet_veronika("Вероніка")
+
+#Урок 10.2. Знайти перше слово
+def first_word(text):
+    for ch in ".,":
+        text = text.replace(ch, " ")
+    words = text.split()
+    return words[0] if words else ""
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
+print('OK')
