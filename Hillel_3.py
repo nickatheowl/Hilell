2f4f1276211674c159a1df015ce2d14fa9409d3b
Veronika <nickatheowl@gmail.com>
2025-08-09 18:53:56 +0300

    # Урок 8.1. - Додати 1 до числа

---
+++ Hillel_3.py
@@ -0,0 +1,11 @@
+# Урок 8.1. - Додати 1 до числа
+def add_one(some_list):
+    number = int(''.join(map(str, some_list)))
+    number += 1
+    return [int(digit) for digit in str(number)]
+
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+assert add_one([0]) == [1], 'Test3'
+assert add_one([9]) == [1, 0], 'Test4'
+print("ОК")
