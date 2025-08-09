3157fca89a4df3da6c87bf510e5cd810cd1f669a
Veronika <nickatheowl@gmail.com>
2025-08-09 18:40:20 +0300

    Урок 7.3 -  Пошук підрядка

--- Hillel_2.py
+++ Hillel_2.py
@@ -70,3 +70,12 @@ print(second_index("test", "t"))
 print(second_index("apple", "p"))
 print(second_index("aaaaa", "aa"))
 
+#Урок 7.4. - Пошук спільних елементів
+def common_elements():
+    multiples_of_3 = {x for x in range(100) if x % 3 == 0}
+    multiples_of_5 = {x for x in range(100) if x % 5 == 0}
+    return multiples_of_3 & multiples_of_5
+
+
+assert common_elements() == {0, 15, 30, 45, 60, 75, 90}
+print("ОК")
