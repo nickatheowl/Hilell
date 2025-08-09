d1e62fb926668dc16a7188d8c648bfe8293bed97
Veronika <nickatheowl@gmail.com>
2025-08-09 18:38:30 +0300

    Урок 7.3 -  Пошук підрядка

--- Hillel_2.py
+++ Hillel_2.py
@@ -54,3 +54,19 @@ else:
         text += '.'
     result = text
 print(result)
+
+# Урок 7.3 -  Пошук підрядка
+def second_index(text: str, some_str: str) -> int | None:
+    first_pos = text.find(some_str)
+    if first_pos == -1:
+        return None
+    second_pos = text.find(some_str, first_pos + len(some_str))
+    return second_pos if second_pos != -1 else None
+
+
+print(second_index("banana", "na"))
+print(second_index("abracadabra", "bra"))
+print(second_index("test", "t"))
+print(second_index("apple", "p"))
+print(second_index("aaaaa", "aa"))
+
