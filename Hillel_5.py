7210cb9ffeaf8cbe05459737442b8a960fe936bb
Veronika <nickatheowl@gmail.com>
2025-08-09 21:02:53 +0300

    #Урок 11.3

--- Hillel_5.py
+++ Hillel_5.py
@@ -41,3 +41,16 @@ assert isgenerator(gen) == True, 'Test0'
 assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
 assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
 assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+
+
+#Урок 11.3 - Перевірка на парність
+
+def is_even(number):
+    return (number & 1) == 0
+
+print(is_even(10))
+print(is_even(7))
+print(is_even(0))
+print(is_even(12345678901234567890))
+print(is_even(12345678901234567891))
+
