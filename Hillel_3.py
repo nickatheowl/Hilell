f24853610e4bf9fb909a877155853339779bc693
Veronika <nickatheowl@gmail.com>
2025-08-09 18:59:31 +0300

    # Урок 8.2 -  Паліандром

--- Hillel_3.py
+++ Hillel_3.py
@@ -9,3 +9,17 @@ assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
 assert add_one([0]) == [1], 'Test3'
 assert add_one([9]) == [1, 0], 'Test4'
 print("ОК")
+
+
+# Урок 8.2 -  Паліандром
+
+import string
+def is_palindrome(text):
+    filtered = ''.join(ch.lower() for ch in text if ch.isalnum())
+    return filtered == filtered[::-1]
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
