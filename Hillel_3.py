19855fe656859b5194d55ec2e657c885cfec21fd
Veronika <nickatheowl@gmail.com>
2025-08-09 19:01:07 +0300

    #Урок 8.3. - Унікальне число

--- Hillel_3.py
+++ Hillel_3.py
@@ -23,3 +23,15 @@ assert is_palindrome('0P') == False, 'Test2'
 assert is_palindrome('a.') == True, 'Test3'
 assert is_palindrome('aurora') == False, 'Test4'
 print("ОК")
+
+#Урок 8.3. - Унікальне число
+
+def find_unique_value(some_list):
+    for num in some_list:
+        if some_list.count(num) == 1:
+            return num
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+print("ОК")
