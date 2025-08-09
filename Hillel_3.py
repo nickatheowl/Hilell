a13e96b3ab94dbeb90ce92da2b3a7ae01c428345
Veronika <nickatheowl@gmail.com>
2025-08-09 19:08:06 +0300

    #Урок 9.2

--- Hillel_3.py
+++ Hillel_3.py
@@ -51,3 +51,15 @@ assert popular_words(
     ['жабки', 'місяцем', 'літа', 'жабка']
 ) == {'жабки': 3, 'місяцем': 1, 'літа': 1, 'жабка': 1}, 'Test1'
 print('OK')
+
+#Урок 9.2 - Різниця між числами
+def difference(*args):
+    if not args:
+        return 0
+    return round(max(args) - min(args), 2)
+
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
+print('OK')
