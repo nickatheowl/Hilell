e5bf411e7f885e1e99c530d55fac4279266f9dd5
Veronika <nickatheowl@gmail.com>
2025-08-14 19:36:03 +0300

    Урок 12.1

--- Hillel_5.py
+++ Hillel_5.py
@@ -54,3 +54,18 @@ print(is_even(0))
 print(is_even(12345678901234567890))
 print(is_even(12345678901234567891))
 
+#Урок 12.1 - Очистити текст від html-тегів
+import codecs
+import re
+
+def delete_html_tags(html_file, result_file='cleaned.txt'):
+    with codecs.open(html_file, 'r', 'utf-8') as file:
+        html = file.read()
+    cleaned_text = re.sub(r'<.*?>', '', html)
+    cleaned_lines = [line.strip() for line in cleaned_text.split('\n') if line.strip()]
+    with codecs.open(result_file, 'w', 'utf-8') as file:
+        file.write('\n'.join(cleaned_lines))
+
+
+# Приклад:
+delete_html_tags('draft.html', 'cleaned.txt')
